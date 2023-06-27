fa789397129ead4cb061ca8cc0e49f09d71e328f
damir1899 <damin_99.99@mail.ru>
2023-06-27 17:32:26 +0600

    GraphQL Django RestAPI

--- graphql_main/schema.py
+++ graphql_main/schema.py
@@ -23,7 +23,116 @@ class Query(graphene.ObjectType):
     def resolve_post_model(self, info):
         return Post.objects.all()
     
+    
+class CreateCategory(graphene.Mutation):
+    class Arguments:
+        new_name = graphene.String(required=True)
+        
+    category = graphene.Field(CategoryModelType)
 
-schema = graphene.Schema(query=Query)
+    def mutate(self, info, new_name):
+            category = Category.objects.create(name=new_name)
+            return CreateCategory(category=category)
+        
+    
+class UpdateCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        update_name = graphene.String()
+        
+    category = graphene.Field(CategoryModelType)
+    
+    def mutate(self, info, id, update_name):
+        category = Category.objects.get(id=id)
+        if update_name:
+            category.name = update_name
+        category.save()
+        return UpdateCategory(category=category)
+            
+            
+class DeleteCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        
+    success = graphene.Boolean()
+    
+    def mutate(self, info, id):
+        category = Category.objects.get(id=id)
+        category.delete()
+        return DeleteCategory(success=True)
+    
+
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        new_image = graphene.String(required=True)
+        new_title = graphene.String(required=True)
+        new_description = graphene.String(required=True)
+        new_category = graphene.Int(required=True)
+        new_location = graphene.String(required=True)
+        
+        
+    post = graphene.Field(PostModelType)
+
+    def mutate(self, info, new_image, new_title, 
+               new_description, new_category, new_location):
+        post = Post.objects.create(image=new_image, 
+                                   title=new_title, 
+                                   description=new_description, 
+                                   category=Category.objects.get(id=new_category), 
+                                   location=new_location)
+        return CreatePost(post=post)
+        
+    
+class UpdatePost(graphene.Mutation):
+    class Arguments:
+        new_id = graphene.ID(required=True)
+        new_image = graphene.String(required=True)
+        new_title = graphene.String(required=True)
+        new_description = graphene.String(required=True)
+        new_category = graphene.Int(required=True)
+        new_location = graphene.String(required=True)
+        
+    post = graphene.Field(PostModelType)
+    
+    def mutate(self, info, new_id, new_image, new_title, 
+               new_description, new_category, new_location):
+        post = Post.objects.get(id=new_id)
+        if new_image:
+            post.image = new_image
+        if new_title:
+            post.title = new_title
+        if new_description:
+            post.description = new_description
+        if new_category:
+            post.category = Category.objects.get(id=new_category)
+        if new_location:
+            post.location = new_location
+        post.save()
+        return UpdatePost(post=post)
+            
+            
+class DeletePost(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        
+    success = graphene.Boolean()
+    
+    def mutate(self, info, id):
+        post = Post.objects.get(id=id)
+        post.delete()
+        return DeletePost(success=True)
+    
+            
+class Mutation(graphene.ObjectType):
+    create_category = CreateCategory.Field()
+    update_category = UpdateCategory.Field()
+    delete_category = DeleteCategory.Field()
+    
+    create_post = CreatePost.Field()
+    update_post = UpdatePost.Field()
+    delete_post = DeletePost.Field()
+    
+    
+schema = graphene.Schema(query=Query, mutation=Mutation)
     
     
---
+++ test.py
@@ -0,0 +1,15 @@
+# CRUD
+
+# Create Read Update Delete
+# POST   GET   PUT   DELETE
+
+
+# class CategoryModelType(DjangoObjectType):
+#     class Meta:
+#         model = Category
+
+
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = "__all__"
